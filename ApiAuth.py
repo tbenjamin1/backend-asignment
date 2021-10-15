df2046855848332e8f33feb4bb534aca4b874584
tbenjamin1 <tbenpollyl@gmail.com>
2021-10-15 12:48:00 +0300

    auth

--- ApiAuth.py
+++ ApiAuth.py
@@ -1,43 +1,58 @@
 
-import json
+# import json
+# import requests
+
+
+# url = "https://reqres.in/api/users/auth/api-key"
+
+# payload = json.dumps({
+#     "name": "ben",
+#     "job": "dev"
+# })
+
+# headers = {
+
+#     "Accept": "application/json",
+#     "Content-Type": "application/json"
+# }
+
+# Response = requests.post(url, headers=headers, data=payload)
+
+# print(Response.json())
+
+# token = Response.get("access_token")
+# refresh = Response.get("refresher_token")
+
+# # when token expires use refresher token
+
+# url = "https://reqres.in/api/users/auth/refresh"
+
+# payload = json.dumps({
+#     {" refresherTokens": refresh}
+# })
+
+# headers = {
+
+#     "Accept": "application/json",
+#     "Content-Type": "application/json"
+# }
+
+# Response = requests.post(url, headers=headers, data=payload)
+
+# print(Response.json())
+
+
 import requests
+from requests.auth import HTTPBasicAuth
+from requests.sessions import session
 
-url = "https://reqres.in/api/users/auth/api-key"
+auth_token = ""
+user_name = "benjamin"
+BASE_URL = "http://localhost:8080"
 
-payload = json.dumps({
-    "name": "ben",
-    "job": "dev"
-})
+session = requests.session()
 
-headers = {
+resp = session.get(BASE_URL +,
+                   auth=HTTPBasicAuth(user_name, auth_token))
 
-    "Accept": "application/json",
-    "Content-Type": "application/json"
-}
-
-Response = requests.post(url, headers=headers, data=payload)
-
-print(Response.json())
-
-token = Response.get("access_token")
-refresh = Response.get("refresher_token")
-
-# when token expires use refresher token
-
-url = "https://reqres.in/api/users/auth/refresh"
-
-payload = json.dumps({
-    {" refresherTokens": refresh}
-})
-
-headers = {
-
-    "Accept": "application/json",
-    "Content-Type": "application/json"
-}
-
-Response = requests.post(url, headers=headers, data=payload)
-
-print(Response.json())
-
-
+print(resp.json())
--- exception.py
+++ exception.py
@@ -1,18 +1,43 @@
 
 
-from requests import exceptions
+# from requests import exceptions
+# from requests.models import HTTPError
 
 
+# try:
+#     file = open('test.txt')
+
+
+# except FileNotFoundError as e:
+#     print(e)
+# except exceptions as e:
+#     print(e)
+# else:
+#     print(file.read())
+#     file.close
+# finally:
+#     print('now its working great...!!')
+
+# http request
+
+
+import requests
+# resp = requests.get('https://reqres.in/api/users/h')
+# try:
+#     resp.raise_for_status()
+# except requests.exceptions.HTTPError:
+#     print("HTTP REQUEST ERROR")
+# else:
+
+#     print(resp)
+
+# connection error
+
+resp = requests.get('https://reqres.in/api/users')
 try:
-    file = open('test.txt')
-
-
-except FileNotFoundError as e:
-    print(e)
-except exceptions as e:
-    print(e)
+    resp.raise_for_status()
+except requests.exceptions.ConnectionError:
+    print("ConnectionError ERROR")
 else:
-    print(file.read())
-    file.close
-finally:
-    print('now its working great...!!')
+
+    print(resp)
